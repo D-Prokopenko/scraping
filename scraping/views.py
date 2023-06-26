b17edf4fc9f6e12c0c5982d95d3ce55b98964663
D-Prokopenko <denis1489@list.ru>
2023-06-26 14:34:09 +0300

    [upd] add search, account

--- scraping/views.py
+++ scraping/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+
+from .forms import FindForm
 from .models import Vacancy
 # Create your views here.
 
@@ -7,7 +9,25 @@ from .models import Vacancy
 class HomeView(View):
     """ Домашняя страница """
     def get(self, request):
-        vacancy = Vacancy.objects.all()
+        form = FindForm()
         return render(request, 'scraping/home.html', {
+            'form': form,
+        })
+
+
+class SearchView(View):
+    """ Страница с вакансиями """
+    def get(self, request):
+        vacancy = []
+        city = request.GET.get('city')
+        language = request.GET.get('language')
+        if city or language:
+            _filter = {}
+            if city:
+                _filter['city__slug'] = city
+            if language:
+                _filter['language__slug'] = language
+            vacancy = Vacancy.objects.filter(** _filter)
+        return render(request, 'scraping/search.html', {
             'vacancy': vacancy,
             })
